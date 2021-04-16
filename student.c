b5eaca88c79bcfe0e49653f08590eaf27d9f5545
menna gamal <menna.eid98@eng-st.cu.edu.eg>
2021-04-16 15:28:15 +0200

    menna

---
+++ student.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct dateofbirth{
+    int day,month,year;
+    };
+struct student{
+    char name[50];
+    int ID;
+    struct dateofbirth DOB;
+    float score;
+}s;
+int main()
+{
+    printf("...welcome to our student information system...\n");
+    printf("please enter the student information\n");
+    printf("name\n");
+    fgets(s.name, sizeof(s.name), stdin);
+    printf("ID\n");
+    scanf("%d",&s.ID);
+    printf("Enter Date of Birth in [D M Y] format: ");
+    scanf("%d%d%d",&s.DOB.day,&s.DOB.month,&s.DOB.year);
+    printf("score\n");
+    scanf("%f",&s.score);
+    printf("Displaying Information:\n");
+    printf("Name: ");
+    printf("%s", s.name);
+    printf("ID: %d\n", s.ID);
+    printf(" Date of birth : %02d/%02d/%02d: %d\n", s.DOB.day,s.DOB.month,s.DOB.year);
+    printf("score:  %.1f\n", s.score);
+
+    return 0;
+}
