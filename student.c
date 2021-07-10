7453d764e48827472f87cf09b8b0d42087e34575
HayatGamal2000 <71998574+HayatGamal2000@users.noreply.github.com>
2021-07-11 01:46:26 +0200

    Update student.c

--- student.c
+++ student.c
@@ -1,3 +1,10 @@
+
+//يارا محمود احمد على          Sec:4   BN:45
+//منة الله جمال عيد               Sec:4    BN:24
+//حياة جمال عبد الهادى         Sec:2    BN:4
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
