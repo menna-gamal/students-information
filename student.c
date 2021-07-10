82b86ed9df93affd1a7f1f8150bdc6d81a57ef08
HayatGamal2000 <71998574+HayatGamal2000@users.noreply.github.com>
2021-07-11 00:14:41 +0200

    Update student.c

--- student.c
+++ student.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
+#include<time.h>
+
 struct dateofbirth{
     int day,month,year;
     };
@@ -9,16 +11,23 @@ struct dateofbirth{
     int ID;
     struct dateofbirth DOB;
     int score;
-<<<<<<< HEAD
 }std;
 
-//menna gamal
+struct node {
+    std data;
+    struct node *next;
+}*head ,*tail,*pre ,*aft,*studentX;
+
+void createList(int n);
+void traverseList();
+
  void readinfo (int  index,std *studentInformation );
 
 int main()
 {
     int num;
-    int key; //to define the way which we will store with
+    int key;  //to define the way which we will store with
+    int ins;   // to know the user will insert or not
     int ins_place;
     int finalNumber;
     printf("...welcome to our student information system...\n\n");
@@ -41,9 +50,13 @@ int main()
     readinfo (i,&allStudents[i]);
     }
     printf("Data stored successfully\n\n");
+    while(1){
 
     printf("Do you want to insert new student ?\n");
+    printf("1:Insert student \n2:Exit \n");
+    scanf("%d",&ins);
 
+    if (ins==1){
             allStudents=realloc(allStudents,num+1);
               finalNumber=num+1;
 
@@ -56,31 +69,52 @@ int main()
               }
             if (ins_place<=num){
             if(ins_place == 1){
+                    clock_t t;
+            t = clock();
 
             for(int i=num; i>0;i--){
                     allStudents[i]=allStudents[i-1];
                 }
                    readinfo (0,&allStudents[0]);
+                   t = clock() -t;
+                   double time_taken = ((double)t)/CLOCKS_PER_SEC;
+            printf("Insertion took %f seconds to execute\n",time_taken);
             }
               else if(ins_place == num+1 ){
-
+                  clock_t t;
+            t = clock();
             finalNumber=num+1;
              readinfo (num,&allStudents[num]);
+                 t = clock() -t;
+                   double time_taken = ((double)t)/CLOCKS_PER_SEC;
+            printf("Insertion took %f seconds to execute\n",time_taken);
             }
             else
             {
+                 clock_t t;
+            t = clock();
                  for(int i=num; i >=ins_place ;i--){
                     allStudents[i]=allStudents[i-1];
                 }
                 readinfo (ins_place-1,&allStudents[ins_place-1]);
+                    t = clock() -t;
+                   double time_taken = ((double)t)/CLOCKS_PER_SEC;
+            printf("Insertion took %f seconds to execute\n",time_taken);
             }
 
             }
+            else{
+                 printf("you entered wrong value\n");
+            }
+    }
+    else if(ins == 2){
+                        break;
+                     }
             else
-            {
-               printf("you entered wrong value\n");
+                {
+               printf("you entered wrong value\n\n");
                 }
-
+    }
     printf("\n\n Displaying Informations:\n");
     for (int i=0; i < finalNumber ;i++){
     printf("Information of student %d \n\n",i+1);
@@ -90,38 +124,17 @@ int main()
     printf("score of the last year: %d\n",allStudents[i].score);
     printf("\n");
     }
-
+        printf("\nThe total number of the students %d\n\n",finalNumber);
+        printf("size of structure =%d\n\n",finalNumber*sizeof(std));
         free(allStudents);
     }
     else if(key == 2)
     {
-        //linked lists
+       createList(num);
     }
     else{
         printf("you entered wrong value\n");
     }
-=======
-}s;
-int main()
-{
-    printf("...welcome to our student information system...\n");
-    printf("please enter the student information\n");
-    printf("name\n");
-    fgets(s.name, sizeof(s.name), stdin);
-    printf("ID\n");
-    scanf("%d",&s.ID);
-    printf("Enter Date of Birth in [D M Y] format: ");
-    scanf("%d%d%d",&s.DOB.day,&s.DOB.month,&s.DOB.year);
-    printf("score\n");
-    scanf("%d",&s.score);
-    printf("Displaying Information:\n");
-    printf("Name: ");
-    printf("%s", s.name);
-    printf("ID: %d\n", s.ID);
-    printf(" Date of birth : %02d/%02d/%02d: %d\n", s.DOB.day,s.DOB.month,s.DOB.year);
-    printf("score:  %d\n", s.score);
->>>>>>> 6d9933a... menna
-
     return 0;
 }
 
@@ -129,7 +142,6 @@ void readinfo (int index , std *studentInformation){
     char temp;
     printf("name of student %d: \n ",(index+1));
     scanf("%c",&temp);   //to avoid compiler to store either enter or null into the string’s first character and string input terminates.
-    //scanf("%[^\n]",&studentInformation->name);
     fgets(studentInformation->name , sizeof(studentInformation->name),stdin);
     printf("ID :\n");
     scanf("%d",&studentInformation->ID);
@@ -140,3 +152,169 @@ void readinfo (int index , std *studentInformation){
     printf("\n");
 }
 
+void createList(int n)
+{
+    struct node *newNode, *temp,*pre,*aft,*studentX;
+    int i;
+    char x;
+    int index;
+
+    head = (struct node *)malloc(sizeof(struct node));
+    if(head == NULL)
+    {
+        printf("Unable to allocate memory.");
+        exit(0);
+    }
+    printf("\n Enter the information of student number 1:\n ");
+    printf("The Name\n");
+    scanf("%c",&x);
+    fgets(head->data.name,sizeof(head-> data .name),stdin);
+    printf("ID\n");
+    scanf("%d",&head-> data .ID);
+    printf("Enter Date of Birth in [D M Y] format: ");
+    scanf("%d%d%d",&head-> data .DOB.day,&head-> data .DOB.month,&head-> data .DOB.year);
+    printf("the score of the last year\n");
+    scanf("%d",&head-> data .score);
+    head-> next = NULL;
+    temp = head;
+    for(i=2; i<= n-1; i++)
+    {
+        newNode = (struct node *)malloc(sizeof(struct node));
+        if(newNode == NULL)
+        {
+            printf("Unable to allocate memory.");
+            break;
+        }
+        printf("\n Enter the information of student number %d: \n", i);
+        printf("the Name\n");
+         scanf("%c",&x);
+    fgets(newNode-> data .name,sizeof(newNode-> data .name),stdin);
+    printf("ID\n");
+    scanf("%d",&newNode-> data.ID);
+    printf("Enter Date of Birth in [D M Y] format: ");
+    scanf("%d%d%d",&newNode-> data.DOB.day,&newNode-> data.DOB.month,&newNode-> data.DOB.year);
+    printf("the score of the last year\n");
+    scanf("%d",&newNode-> data.score);
+
+        newNode->next = NULL;
+        temp->next = newNode;
+        temp = temp->next ;
+    }
+ tail = (struct node *)malloc(sizeof(struct node));
+            temp->next = tail;
+printf("\n Enter the information of student number %d:\n ", i);
+        printf("the Name\n");
+        scanf("%c",&x);
+    fgets(tail-> data .name,sizeof(tail-> data .name),stdin);
+    printf("ID\n");
+    scanf("%d",&tail->data.ID);
+    printf("Enter Date of Birth in [D M Y] format: ");
+    scanf("%d%d%d",&tail->data.DOB.day,&tail->data.DOB.month,&tail->data.DOB.year);
+    printf("the score of the last year\n");
+    scanf("%d",&tail->data.score);
+
+            tail->next = NULL;
+            int order;
+
+while (1){
+    printf("If you want to insert new student enter number 1:\n If you want to exit the program enter 0 :\n ");
+scanf("%d",&order);
+if (order ==1){
+ int k;
+ n=n+1;
+studentX = (struct node *)malloc(sizeof(struct node));
+studentX ->next =NULL;
+printf(" If you want to insert in the begin enter 1 \n If you want to insert in the middle enter 2 \n If you want to insert in the end enter 3 \n");
+scanf("%d", &k);
+if (k == 1){
+        clock_t t;
+t = clock();
+studentX ->next = head;
+ head = studentX;
+ printf("Enter the information of the student that you want to insert \n");
+
+    printf("the Name\n");
+    scanf("%c",&x);
+    fgets(head-> data .name,sizeof(head-> data .name),stdin);
+    printf("ID\n");
+    scanf("%d",&head->data.ID);
+    printf("Enter Date of Birth in [D M Y] format: ");
+    scanf("%d%d%d",&head->data.DOB.day,&head->data.DOB.month,&head->data.DOB.year);
+    printf("score\n");
+    scanf("%d",&head->data.score);
+    t = clock() -t;
+                   double time_taken = ((double)t)/CLOCKS_PER_SEC;
+            printf("Insertion took %f seconds to execute\n",time_taken);
+}
+else if(k == 2){
+     clock_t t;
+t = clock();
+    printf("what is the index ? \n");
+    scanf("%d",&index);
+     pre = head;
+    for(i =0 ; i<index;i++){
+     pre ->next =pre ;
+    pre->next =aft ;
+    studentX->next = aft ;
+    pre->next = studentX;}
+
+    printf("Enter the information of the student that you want to insert \n");
+    printf("the Name\n");
+    scanf("%c",&x);
+    fgets(studentX-> data .name,sizeof(studentX-> data .name),stdin);
+    printf("ID\n");
+    scanf("%d",&studentX->data.ID);
+    printf("Enter Date of Birth in [D M Y] format: ");
+    scanf("%d%d%d",&studentX->data.DOB.day,&studentX->data.DOB.month,&studentX->data.DOB.year);
+    printf("score\n");
+    scanf("%d",&studentX->data.score);
+
+    studentX->next=newNode;
+t = clock() -t;
+                   double time_taken = ((double)t)/CLOCKS_PER_SEC;
+            printf("Insertion took %f seconds to execute\n",time_taken);
+}
+else if (k == 3){
+         clock_t t;
+t = clock();
+tail -> next = studentX;
+tail =studentX;
+printf("Enter the information of the student that you want to insert \n");
+
+    printf("the Name\n");
+    scanf("%c",&x);
+    fgets(tail-> data .name,sizeof(tail-> data.name),stdin);
+    printf("ID\n");
+    scanf("%d",&tail->data.ID);
+    printf("Enter Date of Birth in [D M Y] format: ");
+    scanf("%d%d%d",&tail->data.DOB.day,&tail->data.DOB.month,&tail->data.DOB.year);
+    printf("score\n");
+    scanf("%d",&tail->data.score);
+    t = clock() -t;
+                   double time_taken = ((double)t)/CLOCKS_PER_SEC;
+            printf("Insertion took %f seconds to execute\n",time_taken);
+}
+else {
+    printf("Not Found");
+
+}}
+ else  break;     }
+ if(head == NULL)
+    {
+        printf("List is empty.");
+        return;
+    }
+    temp = head;
+    printf("\n\n Displaying Informations:\n");
+        for(int i=0;i<n;i++){
+    printf(" The informations of student number %d:\n",i+1);
+    printf("The Name:%s",temp->data.name);
+    printf("ID: %d\n", temp->data.ID);
+    printf("Date of birth : %d/%d/%d \n", temp->data.DOB.day,temp->data.DOB.month,temp->data.DOB.year);
+    printf("the score of the last year:  %d\n", temp->data.score);
+     temp=temp->next;
+    }
+    printf("The total number of the students %d\n\n",n);
+    printf("size of structure = %d\n\n ",n*sizeof(std));
+    printf("size of node %d",sizeof(struct node));
+}
